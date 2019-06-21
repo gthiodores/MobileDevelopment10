fd24c38d760c9f45e3448e5556b8761b8d4bbab3
gthiodores <48356058+gthiodores@users.noreply.github.com>
2019-06-21 10:00:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/mobiledevelopment10/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.mobiledevelopment10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String url = "https://wasdlabs.com/";
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyBrowser());
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
